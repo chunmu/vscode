35e7a9194a95116e4f308ec130e387fc8e0f04d7
chunmu.zhang <chunmu.zhang1992@gmail.com>
2025-04-11 21:06:06 +0800

    feat: 设置协议

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,131 @@
 /////// <reference types="node" />
 import * as path from 'path';
-import * as fs from 'original-fs';
-import * as os from 'os';
+import minimist from 'minimist';
 import { fileURLToPath } from 'url';
-import { app, protocol, crashReporter, Menu, contentTracing } from 'electron';
+import { app, contentTracing, protocol } from 'electron';
 import { configurePortable } from './bootstrap-node.js';
 import { product } from './bootstrap-meta.js';
+import { getUserDataPath } from './vs/platform/environment/node/userDataPath.js';
+import { NativeParsedArgs } from './vs/platform/environment/common/argv.js';
 
 // 新版node当前目录的获取方式
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Enable portable support
 const portable = configurePortable(product);
-console.log('xxxxxx')
+
+const args = parseCLIArgs();
+
+// Resolve code cache path
+const codeCachePath = getCodeCachePath();
+
+// Set userData path before app 'ready' event
+const userDataPath = getUserDataPath(args, product.nameShort ?? 'code-oss-dev');
+
+app.setPath('userData', userDataPath);
+
+if (portable && portable.isPortable) {
+	app.setAppLogsPath(path.join(userDataPath, 'logs'));
+}
+
+// Register custom schemes with privileges
+protocol.registerSchemesAsPrivileged([
+	{
+		scheme: 'vscode-webview',
+		privileges: { standard: true, secure: true, supportFetchAPI: true, corsEnabled: true, allowServiceWorkers: true, codeCache: true }
+	},
+	{
+		scheme: 'vscode-file',
+		privileges: { secure: true, standard: true, supportFetchAPI: true, corsEnabled: true, codeCache: true }
+	}
+]);
+
+// Load our code once ready
+app.once('ready', function () {
+	if (args['trace']) {
+		let traceOptions: Electron.TraceConfig | Electron.TraceCategoriesAndOptions;
+		if (args['trace-memory-infra']) {
+			const customCategories = args['trace-category-filter']?.split(',') || [];
+			customCategories.push('disabled-by-default-memory-infra', 'disabled-by-default-memory-infra.v8.code_stats');
+			traceOptions = {
+				included_categories: customCategories,
+				excluded_categories: ['*'],
+				memory_dump_config: {
+					allowed_dump_modes: ['light', 'detailed'],
+					triggers: [
+						{
+							type: 'periodic_interval',
+							mode: 'detailed',
+							min_time_between_dumps_ms: 10000
+						},
+						{
+							type: 'periodic_interval',
+							mode: 'light',
+							min_time_between_dumps_ms: 1000
+						}
+					]
+				}
+			};
+		} else {
+			traceOptions = {
+				categoryFilter: args['trace-category-filter'] || '*',
+				traceOptions: args['trace-options'] || 'record-until-full,enable-sampling'
+			};
+		}
+
+		contentTracing.startRecording(traceOptions).finally(() => onReady());
+	} else {
+		onReady();
+	}
+});
+
+async function onReady() {
+
+	try {
+
+		await startup(codeCachePath);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+function parseCLIArgs(): NativeParsedArgs {
+	return minimist(process.argv, {
+		string: [
+			'user-data-dir',
+			'locale',
+			'js-flags',
+			'crash-reporter-directory'
+		],
+		boolean: [
+			'disable-chromium-sandbox',
+		],
+		default: {
+			'sandbox': true
+		},
+		alias: {
+			'no-sandbox': 'sandbox'
+		}
+	});
+}
+
+function getCodeCachePath(): string | undefined {
+
+	// explicitly disabled via CLI args
+	if (process.argv.indexOf('--no-cached-data') > 0) {
+		return undefined;
+	}
+
+	// running out of sources
+	if (process.env['VSCODE_DEV']) {
+		return undefined;
+	}
+
+	// require commit id
+	const commit = product.commit;
+	if (!commit) {
+		return undefined;
+	}
+
+	return path.join(userDataPath, 'CachedData', commit);
+}
